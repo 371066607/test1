b1e592215ba576411fcedd82cf4d7af26b9efc65
yetonggeng <103116378+371066607@users.noreply.github.com>
2026-05-12 01:59:34 +0800

    feat: add video model

---
+++ flutter_app/lib/models/video_model.dart
@@ -0,0 +1,13 @@
+class VideoModel {
+  final String id;
+  final String title;
+  final String creator;
+  final int likes;
+
+  VideoModel({
+    required this.id,
+    required this.title,
+    required this.creator,
+    required this.likes,
+  });
+}
