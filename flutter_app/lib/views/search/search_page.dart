1d7ea25e82d8a6f57a064d1aadbac9883cab9608
yetonggeng <103116378+371066607@users.noreply.github.com>
2026-05-12 02:00:29 +0800

    feat: add search page

---
+++ flutter_app/lib/views/search/search_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey.shade900,
+            hintText: 'Search videos or users',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
